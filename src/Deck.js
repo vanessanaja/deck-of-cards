947cdfee20d96b35d13449a49332e03b81938511
vanessanaja <vanessacnaja@gmail.com>
2019-06-18 15:27:19 -0700

    add getCard function

---
+++ src/Deck.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from "axios";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
+
+class Deck extends Component{
+  constructor(props){
+    super(props);
+    this.state = { deck: null }
+    this.getCard = this.getCard.bind(this);
+  }
+  async componentDidMount(){
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+    this.setState({ deck: deck.data }); 
+  }
+  async getCard(){
+    let id = this.state.deck.deck_id
+    let cardURL = (`${API_BASE_URL}/${id}/draw/`);
+    let cardRes = await axios.get(cardURL);
+    console.log(cardRes.data);
+  }
+  render(){
+    return (
+      <div>
+        <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
+      </div>
+    );
+  }
+}
+
+export default Deck;
