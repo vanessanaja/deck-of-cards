25088e7a1953a3cfdf70112961873234be879388
vanessanaja <vanessacnaja@gmail.com>
2019-06-19 21:47:38 -0700

    display cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from './Card';
 import axios from "axios";
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
@@ -38,10 +39,14 @@ class Deck extends Component{
   
     
   render(){
+    const cards = this.state.drawn.map(c => (
+      <Card name={c.name} image={c.image}/>
+    ));
     return (
       <div>
         <h1>Card Dealer</h1>
         <button onClick={this.getCard}>Get Card!</button>
+        {cards}
       </div>
     );
   }
