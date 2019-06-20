46b64d9d028fc9a870bb77c007d9987771cb9ea9
vanessanaja <vanessacnaja@gmail.com>
2019-06-20 12:25:07 -0700

    center and stack cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import './Deck.css';
 import axios from "axios";
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
@@ -40,13 +41,15 @@ class Deck extends Component{
     
   render(){
     const cards = this.state.drawn.map(c => (
-      <Card name={c.name} image={c.image}/>
+      <Card name={c.name} image={c.image} key={c.id}/>
     ));
     return (
       <div>
         <h1>Card Dealer</h1>
         <button onClick={this.getCard}>Get Card!</button>
-        {cards}
+        <div className = 'Deck-card-area'>
+          {cards}
+        </div>
       </div>
     );
   }
