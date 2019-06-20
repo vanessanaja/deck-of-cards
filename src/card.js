c9b521df73c8b9b8a92c53f4ecabacceb834fb49
vanessanaja <vanessacnaja@gmail.com>
2019-06-20 12:37:26 -0700

    tranform and rotate individual cards

--- src/card.js
+++ src/card.js
@@ -3,8 +3,16 @@ import './Card.css';
 
 class Card extends Component{
   render(){
+    let angle = Math.random() * 90 - 45;
+    let xPos = Math.random() * 40 - 20;
+    let yPos = Math.random() * 40 - 20;
+    let transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
     return(
-      <img className='Card' src={this.props.image} alt={this.props.name}/>
+      <img 
+        style={{transform: transform}} 
+        className='Card' 
+        src={this.props.image} 
+        alt={this.props.name} />
     )
   }
 }
